5eeff17e983d76e55974e7d8f528eb1bb7acde4c
Asif <connectasifshaik@gmail.com>
2025-06-07 18:10:50 +0530

    Initial Commit

---
+++ SwiftUI Fundamentals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI Fundamentals
+//
+//  Created by Asif Shaik on 07/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI Fundamentals/SwiftUI_FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FundamentalsApp.swift
+//  SwiftUI Fundamentals
+//
+//  Created by Asif Shaik on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
